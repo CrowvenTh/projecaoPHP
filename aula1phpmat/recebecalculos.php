6b9209ba764a9c86c7864fd9e30213037e1d78e5
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2024-08-13 11:03:41 -0300

    implementando laço de repetição

--- aula1phpmat/recebecalculos.php
+++ aula1phpmat/recebecalculos.php
@@ -1,10 +1,17 @@
 <?php
     $v = $_POST['valor1'];
-    if($v > 20){
-        echo('O valor '.$v.' é maior que 20');
+
+    for($cont = 1; $cont < $v; $cont++){
+        echo('<p>Valor atual: '.$cont);
+    }
+/* 
+if($v > 20){
+    echo('O valor '.$v.' é maior que 20');
     } else if ($v < 20){
         echo('O valor '.$v.' é menor que 20');
     } else {
         echo('O valor '.$v.' é igual a 20');
     }
+    */
 ?>
+    

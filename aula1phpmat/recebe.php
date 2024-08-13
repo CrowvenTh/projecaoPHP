005698e413d6d181103976899b00819761eae25b
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2024-08-13 10:38:52 -0300

    add backup de forms

---
+++ aula1phpmat/recebe.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $vnm = $_POST['nm'];
+    echo('ola mundo <br>');
+    echo('O nome digitado foi: '.$vnm);
+    ?>
+</body>
+</html>

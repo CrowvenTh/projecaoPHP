bd58cdff1685961fcaee8b58fcbcdf27664fb8e0
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2024-08-13 11:12:20 -0300

    Update recebecalculos.php

--- aula1phpmat/recebecalculos.php
+++ aula1phpmat/recebecalculos.php
@@ -1,10 +1,21 @@
 <?php
     $v = $_POST['valor1'];
 
+    $dobro = $v * 2;
+    echo('O dobro de '.$v.' é igual a '.$dobro);
+
+    $bol = $v;
+    if($bol % 2 != 0){
+        echo('<p> O número '.$bol.' é impar </p>');
+    } else {
+        echo('<p> O número '.$bol.' é par </p>');
+
+    }
+
     for($cont = 1; $cont < $v; $cont++){
         echo('<p>Valor atual: '.$cont);
     }
-/* 
+    /* 
 if($v > 20){
     echo('O valor '.$v.' é maior que 20');
     } else if ($v < 20){

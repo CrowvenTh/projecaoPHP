8b07dbc66d512394ee19e66ba6e63dafb21637bf
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2024-08-13 10:55:13 -0300

    logica condicional em cadeia

--- aula1phpmat/recebecalculos.php
+++ aula1phpmat/recebecalculos.php
@@ -2,7 +2,9 @@
     $v = $_POST['valor1'];
     if($v > 20){
         echo('O valor '.$v.' é maior que 20');
+    } else if ($v < 20){
+        echo('O valor '.$v.' é menor que 20');
     } else {
-        echo('O valor '.$v.' não é maior que 20');
+        echo('O valor '.$v.' é igual a 20');
     }
 ?>

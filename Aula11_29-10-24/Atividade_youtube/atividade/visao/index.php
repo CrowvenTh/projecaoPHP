3b6f791ed4247cebc5287cf271723dc9027a8fb8
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2024-10-29 09:54:59 -0300

    Atividade youtube

---
+++ Aula11_29-10-24/Atividade_youtube/atividade/visao/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade Video</title>
+</head>
+<body>
+    <form>
+        Nome do video
+        <input type="text" name="inNome" placeholder="insira o nome do video">
+
+        link
+        <input type="url" name="linkVideo"placeholder="Insira o link do video">
+
+        <input type="submit"> Cadastrar
+    </form>
+</body>
+</html>
---
+++ Aula11_29-10-24/Atividade_youtube/guia.md
@@ -0,0 +1,5 @@
+# Atividade PHP
+
+![alt text](image.png)
+
+Criar formulario que seja possivel enviar nome e  link de um video no youtube com metodo listar em uma página separada, a página deve conter o card do video em minitatura
